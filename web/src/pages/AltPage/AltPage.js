18ab966664449f3c9c941a52ca264d57aeddc37a
Michael Gingras <mcg79@cornell.edu>
2021-09-06 21:26:26 -0600

    glow

--- web/src/pages/AltPage/AltPage.js
+++ web/src/pages/AltPage/AltPage.js
@@ -2,21 +2,21 @@ import Logo from '../../assets/logo_bland.svg'
 
 const AltPage = () => {
   return (
-    <div className="bg-gray-100 bg-gradient-to-b from-red-50 to-indigo-200">
+    <div className="bg-black">
       <div className="max-w-screen-xl mx-auto">
         <div className="py-12 flex justify-end -mb-24">
-          <Logo className="self-start h-20 w-20 sm:h-80 sm:w-80 fill-current text-blue-900" />
+          <Logo className="self-start h-20 w-20 sm:h-80 sm:w-80 fill-current text-gray-300" />
         </div>
 
-        <div className="py-12 border-b-2 border-blue-900">
-          <h3 className="text-2xl font-semibold text-blue-900 mb-2">
+        <div className="py-12 border-b-2 border-white">
+          <h3 className="text-2xl font-semibold text-white mb-2">
             From the Office of
           </h3>
-          <h1 className="text-8xl font-bold text-blue-900">
+          <h1 className="text-8xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-pink-600 to-purple-600">
             The Standard <br /> Audio Company
           </h1>
         </div>
-        <div className="grid grid-cols-3 gap-2 text-blue-900 mt-4 leading-5 pb-12">
+        <div className="grid grid-cols-3 gap-2 text-white opacity-50 mt-4 leading-5 pb-12">
           <p>
             Hello, and welcome to your first taste of the Standard Audio
             Company. You will be seeing quite a bit of us around here. We are a
@@ -35,9 +35,35 @@ const AltPage = () => {
           </p>
         </div>
         <div className="flex items-center justify-center pb-24 flex-col">
-          <button className="text-lg bg-blue-900 text-white py-4 rounded-full w-64 mx-auto">
-            Waitlist
-          </button>
+          <div className="flex items-start justify-center">
+            <div className="relative group">
+              <div className="absolute -inset-0.5 bg-gradient-to-r from-pink-600 to-purple-600 rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
+              <button className="relative px-7 py-4 bg-black rounded-lg leading-none flex items-center divide-x divide-gray-600">
+                <span className="flex items-center space-x-5">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6 text-pink-600 -rotate-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M19.428 15.428a2 2 0 00-1.022-.547l-2.387-.477a6 6 0 00-3.86.517l-.318.158a6 6 0 01-3.86.517L6.05 15.21a2 2 0 00-1.806.547M8 4h8l-1 1v5.172a2 2 0 00.586 1.414l5 5c1.26 1.26.367 3.414-1.415 3.414H4.828c-1.782 0-2.674-2.154-1.414-3.414l5-5A2 2 0 009 10.172V5L8 4z"
+                    />
+                  </svg>
+                  <span className="pr-6 text-gray-100">
+                    MXTapes Release 11.2021
+                  </span>
+                </span>
+                <span className="pl-6 text-indigo-400 group-hover:text-gray-100 transition duration-200">
+                  claim early &rarr;
+                </span>
+              </button>
+            </div>
+          </div>
           <p className="text-sm underline mt-2 text-blue-900 cursor-pointer">
             Join our Discord
           </p>

9fec49fd10324bbb86af14fb686e4e88ef48031c
Michael Gingras <mcg79@cornell.edu>
2021-11-15 07:34:10 -0700

    pulling out some pages

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -3,8 +3,6 @@ import EditTapePage from './pages/EditTapePage/EditTapePage'
 import ListeningRoomPage from './pages/ListeningRoom/ListeningRoomPage'
 import TapesClaimPage from './pages/TapeClaimPage/TapesClaimPage'
 import DemoPage from './pages/DemoPage/DemoPage'
-import AboutPage from './pages/AboutPage/AboutPage'
-import FlexTest from './pages/FlexTest/FlexTest'
 
 const Routes = () => {
   return (
@@ -14,10 +12,8 @@ const Routes = () => {
       <Route path="/tapes" page={TapesPage} name="tapes" />
       <Route path="/tapes/{id:Int}/claim" page={TapesClaimPage} name="claims" />
       <Route path="/den/{id:Int}" page={ListeningRoomPage} name="listeningRoom" />
-      <Route path="/flex" page={FlexTest} name="flex" />
-      <Route path="/" page={HomePage} name="home" />
-      <Route path="/about" page={AboutPage} name="about" />
       <Route path="/demo/{id:Int}" page={DemoPage} name="demo" />
+      <Route path="/" page={HomePage} name="home" />
       <Route notfound page={NotFoundPage} />
     </Router>
   )

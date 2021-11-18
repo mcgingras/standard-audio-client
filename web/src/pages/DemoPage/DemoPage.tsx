aed73841d00b9ff41518791dbfaddd0ee38dbdef
Michael Gingras <mcg79@cornell.edu>
2021-11-17 22:58:50 -0700

    css cleanup and demo page fully on spec to tailwind

--- web/src/pages/DemoPage/DemoPage.tsx
+++ web/src/pages/DemoPage/DemoPage.tsx
@@ -4,227 +4,60 @@ import BareScene from '../../components/Three/Scenes/BareScene'
 
 export function Underlay() {
   return (
-    <div
-      className="absolute h-full w-full inline-flex flex-col p-10"
-      style={{
-        top: 0,
-        left: 0,
-        alignItems: 'flex-start',
-        justifyContent: 'flex-start',
-        pointerEvents: 'none',
-      }}
-    >
-      <div
-        className="w-full p-0 inline-flex flex-row items-center justify-center"
-      >
-        <p
-          style={{
-            fontFamily: "'Antonio', sans-serif",
-            flex: '1 1 0%',
-            height: 30,
-            fontSize: 30,
-            fontWeight: 700,
-            lineHeight: '30px',
-            color: 'black',
-            letterSpacing: -2,
-          }}
-        >
+    <div className="absolute h-full w-full inline-flex flex-col p-10 items-start justify-start top-0 left-0">
+      <div className="w-full p-0 inline-flex flex-row items-center justify-center">
+        <p className="flex-1 h-8 text-3xl text-blac font-bold">
           MXTAPES
         </p>
-        <div className="w-2.5" />
-        <p
-          className="full text-black text-center text-xs h-3"
-          style={{
-            flex: '1 1 0%',
-            lineHeight: '12px',
-          }}
-        >
-          DIVINE NAMES
-        </p>
-        <div className="w-2.5" />
-        <p
-          className="full text-black text-center text-xs h-3"
-          style={{
-            flex: '1 1 0%',
-          }}
-        >
-          CREATIONS
-        </p>
-        <div className="w-2.5" />
-        <p
-          className="full text-black text-center text-xs h-3"
-          style={{
-            flex: '1 1 0%',
-            lineHeight: '12px',
-          }}
-        >
-          FORMATIONS
-        </p>
-        <div className="w-2.5" />
-        <p
-          className="full text-black text-center text-xs h-3"
-          style={{
-            flex: '1 1 0%',
-            lineHeight: '12px',
-          }}
-        >
-          SUBSTANCES
-        </p>
-        <div className="w-2.5" />
-        <p
-          className="full text-black text-center text-xs h-3"
-          style={{
-            flex: '1 1 0%',
-            lineHeight: '12px',
-          }}
-        >
-          MATTER
-        </p>
-        <div className="w-2.5" />
-        <p
-          style={{
-            flex: '1 1 0%',
-            height: 30,
-            fontSize: 30,
-            lineHeight: '30px',
-            textAlign: 'right',
-            color: 'black',
-          }}
-        >
-          ⎑
-        </p>
       </div>
-      <div style={{ height: 60 }} />
-      <div
-        style={{
-          width: '100%',
-          padding: 0,
-          display: 'inline-flex',
-          flexDirection: 'row',
-          alignItems: 'flex-start',
-          justifyContent: 'center',
-        }}
-      >
-        <p
-          style={{
-            flex: '1 1 0%',
-            height: '100%',
-            fontSize: 12,
-            lineHeight: '1.5em',
-            color: 'black',
-          }}
-        >
+      <div className="h-14" />
+      <div className="w-full p-0 inline-flex flex-row items-start justify-center">
+        <p className="flex-1 h-full text-xs leading-4 text-black">
           <b>Dark Night Demo Tapes</b>
           <br />
           0x40e2a3cd2a924015fC13088dA1bD64999C2F5DD5
           <br />
           <b>—</b>
         </p>
-        <div style={{ width: 10 }} />
-        <p
-          style={{
-            transform: 'rotate3d(0, 0, 1, 90deg) translate3d(100%,10px,0)',
-            transformOrigin: 'right',
-            fontSize: 12,
-            fontWeight: 700,
-            lineHeight: '100%',
-            textAlign: 'right',
-            color: 'black',
-            whiteSpace: 'nowrap',
-          }}
-        >
-          DRAG POINTER &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ●
-        </p>
+        <div className="w-2.5" />
       </div>
-      <div style={{ height: 10 }} />
-      <div
-        className="w-full p-0 inline-flex flex-row items-end justify-center"
-        style={{
-          fontFamily: "'Antonio', sans-serif",
-          flex: '1 1 0%',
-        }}
-      >
+      <div className="h-2.5" />
+      <div className="w-full flex-1 p-0 inline-flex flex-row items-end justify-center" >
         <p
+          className="flex-1 text-black m-0"
           style={{
-            flex: '1 1 0%',
             fontSize: 250,
             lineHeight: '1em',
-            color: 'black',
-            margin: 0,
             letterSpacing: -10,
           }}
         >
           III
         </p>
-        <div style={{ width: 10 }} />
+        <div className="w-2.5" />
         <p
+          className="flex-1 text-black m-0 text-right"
           style={{
-            flex: '1 1 0%',
             fontSize: 250,
             lineHeight: '100%',
-            textAlign: 'right',
-            color: 'black',
-            margin: 0,
             letterSpacing: -10,
           }}
         >
           014
         </p>
       </div>
-      <div style={{ height: 60 }} />
-      <div
-        style={{
-          pointerEvents: 'all',
-          width: '100%',
-          padding: 0,
-          display: 'inline-flex',
-          flexDirection: 'row',
-          alignItems: 'flex-end',
-          justifyContent: 'center',
-        }}
-      >
-        <p
-          className="full"
-          style={{
-            whiteSpace: 'nowrap',
-            flex: '1 1 0%',
-            fontSize: 12,
-            lineHeight: '1.5em',
-            color: 'black',
-          }}
-        >
+      <div className="h-14" />
+      <div className="w-full p-0 inline-flex flex-row items-end justify-center">
+        <p className="w-full whitespace-nowrap flex-1 text-xs leading-6 text-black">
           <b>Series III</b>
           <br />
           Color Mapped
         </p>
-        <div style={{ width: 10 }} />
-        <p
-          className="full"
-          style={{
-            fontFamily: "'Antonio', sans-serif",
-            flex: '1 1 0%',
-            fontSize: 16,
-            fontWeight: 700,
-            lineHeight: '1em',
-            textAlign: 'center',
-            color: 'black',
-            letterSpacing: -0.5,
-            whiteSpace: 'nowrap',
-          }}
-        >
+        <div className="w-2.5" />
+        <p className="w-full flex-1 text-base font-bold leading-none text-center text-black whitespace-nowrap">
           HOVER FOR STATS
         </p>
-        <div style={{ width: 10 }} />
-        <p
-          className="full"
-          style={{
-            flex: '1 1 0%',
-            fontSize: 12,
-            lineHeight: '1em',
-            textAlign: 'right',
-            color: 'black',
-          }}
-        ></p>
+        <div className="w-2.5" />
+        <p className="w-full flex-1 text-xs leading-none text-right text-black"></p>
       </div>
     </div>
   )
